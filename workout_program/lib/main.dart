f26e155a2848700bfc14c820d40d4d8ad027c2a3
Vaishnavi Singari <ssvd02@gmail.com>
2023-07-16 17:29:45 -0400

    Added a button

--- workout_program/lib/main.dart
+++ workout_program/lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -98,6 +98,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 hintText: 'Enter a workout',
               ),
             ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
           ],
         ),
       ),
