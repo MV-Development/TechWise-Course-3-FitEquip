08b856edeb6915bbe0962c5b10578a409fe03f30
Vaishnavi Singari <ssvd02@gmail.com>
2023-07-24 12:15:12 -0400

    Added logo and chnaged color scheme

--- workout_program/lib/main.dart
+++ workout_program/lib/main.dart
@@ -19,10 +19,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Workout App',
+      //title: 'Workout App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFF552196),
       ),
       home: const MyHomePage(title: 'Workout app'),
     );
@@ -60,7 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: null,
+        leading: Image.asset('assets/FitEquipLogo.png'),
       ),
       body: Center(
         child: Column(
@@ -73,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     items: items.map((String items) {
                       return DropdownMenuItem(
                         value: items,
-                        child: Text(items),
+                        child: Text(items, style: TextStyle(color: items == dropdownvalue ? Colors.white : null)),
                       );
                     }).toList(),
                     onChanged: (String? newValue) {
@@ -89,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Text('Next'),
             ),
-            Text(text, style: const TextStyle(fontSize: 14))
+            Text(text, style: const TextStyle(fontSize: 14, color:Colors.white))
           ],
         ),
       ),
