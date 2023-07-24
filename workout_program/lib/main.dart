67e4697ef3c7ec9d4a68f6bde081cebc36a81d47
Daniel Zombek <djzombek@gmail.com>
2023-07-24 19:19:01 -0400

    updated merge

--- workout_program/lib/main.dart
+++ workout_program/lib/main.dart
@@ -19,11 +19,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //title: 'Workout App',
+      title: 'Workout App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFF552196),
       ),
       home: const MyHomePage(title: 'Workout app'),
     );
@@ -59,47 +58,72 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: null,
-        leading: Image.asset('assets/FitEquipLogo.png'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.only(left: 500.0, right: 500.0),
-                child: DropdownButton(
-                    value: dropdownvalue,
-                    items: items.map((String items) {
-                      return DropdownMenuItem(
-                        value: items,
-                        child: Text(items,
-                            style: TextStyle(
-                                color: items == dropdownvalue
-                                    ? Colors.white
-                                    : null)),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownvalue = newValue!;
-                      });
-                    })),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                getData(dropdownvalue.toLowerCase());
-                print('button pressed!');
-              },
-              child: const Text('Next'),
-            ),
-            Text(text,
-                style: const TextStyle(fontSize: 14, color: Colors.white))
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          leading: Image.asset('assets/FitEquipLogo.png'),
         ),
-      ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [exerciseList(), equipList()],
+          ),
+        ));
+  }
+
+  Column exerciseList() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          DropdownButton(
+              value: dropdownvalue,
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              }),
+          const SizedBox(height: 20),
+          selectExercise(),
+          Text(text, style: const TextStyle(fontSize: 14)),
+        ]);
+  }
+
+  Column equipList() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          DropdownButton(
+              value: dropdownvalue,
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              }),
+          const SizedBox(height: 20),
+          selectExercise(),
+          Text(text, style: const TextStyle(fontSize: 14)),
+        ]);
+  }
+
+  ElevatedButton selectExercise() {
+    return ElevatedButton(
+      onPressed: () {
+        getData(dropdownvalue.toLowerCase());
+        print('button pressed!');
+      },
+      child: const Text('Next'),
     );
   }
 }
