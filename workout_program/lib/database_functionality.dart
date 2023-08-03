a9c0888435826065b8525d35242623a3c0b78c3c
onemindnolimit <mikevisa4500x@gmail.com>
2023-08-02 20:33:03 -0400

    Made alterations to database_functionality.dart

--- workout_program/lib/database_functionality.dart
+++ workout_program/lib/database_functionality.dart
@@ -1,16 +1,31 @@
-class InstanceData {
-  var bodyPartList = <String>[];
-  var equipmentList = <String>[];
-  var exerciseList = <String>[];
-}
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore database = FirebaseFirestore.instance;
 
 
-/*
+
+get_equipment(String muscle, int doc){
+    database.collection(muscle).get().then(
+      (querySnapshot) {
+        return querySnapshot.docs[doc].get("Equipment");
+        }, onError: (e) => print("Error completing: $e"),
+    );
+  }
+
+get_exercise_name(String muscle, int doc){
+    database.collection(muscle).get().then(
+      (querySnapshot) {
+        return querySnapshot.docs[doc].get("Name");
+        }, onError: (e) => print("Error completing: $e"),
+    );
+  }
+
 get_exercises() {
-  exerciseList = [];
   print('');
   for (var muscle in muscleList) {
-    firestore.collection(muscle).get().then(
+    database.collection(muscle).get().then(
       (querySnapshot) {
         for (var doc in querySnapshot.docs) {
           if (hasEquipment(doc.get("Equipment")) &&
