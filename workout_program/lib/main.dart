1c8139b5a1f7a00a2d88efe639906b60ed47a18e
Daniel Zombek <djzombek@gmail.com>
2023-08-09 17:33:08 -0400

    created register page

--- workout_program/lib/main.dart
+++ workout_program/lib/main.dart
@@ -57,6 +57,13 @@ class _LoginPage extends State<LoginPage> {
         child: Column (
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget> [
+            InkWell (
+              child: Text('Register'),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => RegisterPage()));
+              }
+            ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal:300, vertical: 15),
               child: TextField(
@@ -87,6 +94,74 @@ class _LoginPage extends State<LoginPage> {
                 onPressed: () {
                   Navigator.push(
                     context, MaterialPageRoute(builder: (context) => PartSelect()));
+                  print('button pressed!');
+                },
+                child: const Text('Next'),
+              )
+              ),
+          ]
+        )
+      )
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+  @override
+  State<RegisterPage> createState() => _RegisterPage();
+}
+
+class _RegisterPage extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Register'),
+        leading: Image.asset('assets/FitEquipLogo.png')
+      ),
+      body: Center (
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            InkWell (
+              child: Text('Login'),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => LoginPage(title: 'FitEquip')));
+              }
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:300, vertical: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Enter valid email, ex: abcd@gmail.com',
+                )
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:300, vertical: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter valid password',
+                )
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 500, vertical: 15),
+              child:  ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => LoginPage(title: 'FitEquip')));
                   print('button pressed!');
                 },
                 child: const Text('Next'),
